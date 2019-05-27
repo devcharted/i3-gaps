82979088380b2c0880a241b288fce42cb42fb518
Grzegorz Zieba <grzegorz.zieba.ext@gigaset.com>
2019-05-27 14:36:58 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo dnf install feh light pavucontrol network-manager-applet adobe-source-code-pro-fonts rofi ranger dunst zsh
+sudo dnf install feh light pavucontrol network-manager-applet adobe-source-code-pro-fonts rofi ranger dunst zsh lxappearance
 
