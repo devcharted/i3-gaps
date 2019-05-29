ebb3e40377f72f0db0e487ab9843e13d3735165a
Grzegorz Zieba <grzegorz.zieba.ext@gigaset.com>
2019-05-29 14:41:07 +0200

    Add dunst

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo dnf copr enable gregw/i3desktop  
-sudo dnf install i3-gaps feh light pavucontrol network-manager-applet adobe-source-code-pro-fonts rofi ranger dunst zsh lxappearance
+sudo dnf install i3-gaps feh light pavucontrol network-manager-applet adobe-source-code-pro-fonts rofi ranger dunst zsh lxappearance xcompmgr
 
