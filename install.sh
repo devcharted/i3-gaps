ef5b56a673645475200437dc0380667186b09779
Grzegorz Zieba <grzegorz.zieba.ext@gigaset.com>
2019-06-04 10:02:05 +0200

    Updati install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-sudo dnf copr enable gregw/i3desktop  
-sudo dnf install i3-gaps feh light pavucontrol network-manager-applet adobe-source-code-pro-fonts rofi ranger dunst zsh lxappearance xcompmgr xautolock pulseaudio-utils
+sudo dnf copr enable gregw/i3desktop
+sudo dnf install i3-gaps feh light pavucontrol network-manager-applet \
+	adobe-source-code-pro-fonts rofi ranger dunst zsh lxappearance \
+	xcompmgr xautolock pulseaudio-utils gdouros-symbola-fonts
