66375317e2f5aea49a8deda17b946eb2939bf48f
Grzegorz Zieba <grzegorz.zieba.ext@gigaset.com>
2019-07-05 11:26:59 +0200

    Install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-yay -S ttf-liberation ttf-gelasio-ib ttf-carlito ttf-caladea ttf-selawik noto-fonts
+yay -Syy adobe-source-sans-pro-fonts adobe-source-serif-pro-fonts \
+       	alsa-utils awesome-terminal-fonts dunst feh fzf git glances i3-gaps i3blocks \
+       	i3lock-fancy-rapid-git i3status light lightdm lightdm-webkit2-greeter \
+	nerd-fonts-complete network-manager-applet pavucontrol powerline-fonts-git \
+	ranger rofi rxvt-unicode sxiv ttf-caladea ttf-carlito ttf-font-awesome \
+	ttf-freefont ttf-gelasio-ib ttf-liberation ttf-linux-libertine ttf-roboto \
+	ttf-selawik ttf-symbola udiskie urxvt-perls usbutils vim vim-spell-pl \
+	volumeicon xcompmgr yay zsh zsh-autosuggestions zsh-completions
+
